69597875f7d00f3c585933e61e4fad2716d8c5b7
佐藤正康 <satohmsys@gmail.com>
2018-10-14 23:59:13 +0900

    update コーディング用

--- src/components/common/HeaderParts.vue
+++ src/components/common/HeaderParts.vue
@@ -1,11 +1,13 @@
 <template>
-  <header id="header">
-      <router-link tag="h1" to="/" exact><a>ホーム</a></router-link>
-      <nav>
-        <ul>
-          <router-link tag="li" to="/categories" exact><a>カテゴリー</a></router-link>
-        </ul>
-      </nav>
+  <header id="header" class="siteHeader">
+      <div class="wrap">
+        <router-link tag="h1" class="siteHeader__logo" to="/" exact><a><img src="../../assets/img/logo.svg" alt=""></a></router-link>
+        <!-- <nav>
+          <ul>
+            <router-link tag="li" to="/categories" exact><a>カテゴリー</a></router-link>
+          </ul>
+        </nav> -->
+      </div>
   </header>    
 </template>
 
@@ -15,6 +17,53 @@
   }
 </script>
 
-<style>
-  /* CSS */
+<style lang='scss'>
+  body{
+    margin: 0;
+  }
+  .siteHeader{
+    height: 90px;
+    line-height: 1;
+    display: flex;
+    align-items: center;
+    position: relative;
+    background: #6DB3BF;
+    background-image: linear-gradient(to left bottom, #4799B7, #7DC9DC );
+
+
+    &__logo{
+      margin-top: 0;
+      margin-bottom: 0;
+    }
+
+    &:before{
+      content:  " ";
+      width: 100%;
+      height: 100%;
+      transform-origin: center center ;
+      transform: skewY( 0.5deg );    
+      position: absolute;
+      z-index: -1;
+      top: 0;
+      right: 0;
+      left: 0;
+      background: #00D1E5;
+
+    }    
+    &:after{
+      content:  " ";
+      width: 100%;
+      height: 200%;
+      transform-origin: center center ;
+      transform: skewY( -0.5deg );
+      position: absolute;
+      z-index: 1;
+      top: -100%;
+      right: 0;
+      left: 0;
+      background-image: linear-gradient(to right, #488DA7, #22556E );
+
+    }
+  }
+
 </style>
