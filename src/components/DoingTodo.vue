74903c972ef1e04e64c8353a6f0bb8abe2f4e5c1
佐藤正康 <satohmsys@gmail.com>
2018-12-04 14:17:07 +0900

    update タスクコンポーネントマークアップ

--- src/components/DoingTodo.vue
+++ src/components/DoingTodo.vue
@@ -1,11 +1,23 @@
 <template>
-  <div :id="todo._id">
-    <p>{{ todo.name }}</p>
-    <p>{{ this.categoryName() }}</p>
-    <p class="todo_elapsed_time">{{ this.h + ':' + this.m + ':' + this.s  }}</p>
-    <button class="startBtn" v-on:click="start()" :disabled="isDisabedStartBtn()">開始</button>
-    <button class="stopBtn" v-on:click="stop()" :disabled="isDisabedStopBtn()">中断</button>
-    <button class="doneBtn" v-on:click="done()">完了</button>
+  <div :id="todo._id" class="todo">
+    <header class="todo__index">{{ todo.name }}</header>
+    <footer class="todo__footer">
+      <div class="todo__footer__col">
+        <ul class="todo__categories">
+          <li class="todo__category label label--category">
+            {{ this.categoryName() }}
+          </li>
+        </ul>
+        <p class="todo__elapsedTime">経過時間 <span class="elapsedTime">{{ this.h + ':' + this.m + ':' + this.s  }}</span></p>
+      </div>
+      <div class="todo__footer__col">
+        <div class="todo__buttons buttons">
+            <button class="button button--start startBtn" v-on:click="start()" :disabled="isDisabedStartBtn()">開始</button>
+            <button class="button button--stop stopBtn" v-on:click="stop()" :disabled="isDisabedStopBtn()">中断</button>
+            <button class="button button--done doneBtn" v-on:click="done()">完了</button>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -94,5 +106,74 @@
   }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  //////////////////////
+  // color
+  /////////////////////
+  $color_border: #BBBBBB;
+  $color_primary: #22556E;
+  $paleblue: #F6FAFB;
+
+  .todo{
+      padding: 20px;
+      list-style: none;
+      border-radius: 2px;
+      background-color: $paleblue;
+
+      &__index{
+        font-size: 2.6rem;
+        font-weight: bold;
+
+        @media screen and ( max-width: 640px ){
+          font-size: 2rem;
+        } 
+      }
+
+      &:not( :last-child ){
+        margin-top: 20px;
+      }
+
+
+      &__footer{
+        margin-top: 40px;
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-between;
+
+        @media screen and ( max-width: 640px ){
+          margin-top: 30px;
+          align-items: flex-start;
+          flex-direction: column;
+        }         
+      }
+
+      //////カテゴリラベル
+      &__categories{
+        padding: 0;
+        margin: 0;
+      }
+
+      //////経過時間
+      &__elapsedTime{
+        line-height: 1;
+        margin-top: 20px;
+        margin-bottom: 0;
+        letter-spacing: 0.025em;
+
+        .elapsedTime{
+          letter-spacing: 0.0025em;
+          font-family: 'Barlow',sans-serif;
+          font-weight: bold;
+        }
+      }
+
+
+      ////// button
+      &__buttons{
+        @media screen and ( max-width: 640px ){
+          margin-top: 20px;
+        }           
+      }
+  }
+
 </style>
