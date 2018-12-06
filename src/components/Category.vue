df05fca361c365f5d620f602755573bf027c5814
佐藤正康 <satohmsys@gmail.com>
2018-12-06 10:20:24 +0900

    updateカテゴリ登録画面

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,16 +1,30 @@
 <template>
-	<div>
-		<p>カテゴリー登録してね</p>
-		<input type="text" name="name" v-model="name">
-		<button v-on:click="add()">送信</button>
-		<div class="createdCategory">
-			<ul class="categoryList">
-				<li class="category" v-for="(category) in this.categories" :key="category._id">
-					{{ category.name }}
-				</li>
-			</ul>
-		</div>
-	</div>
+	<main class="main">
+    <div class="wrap wrap--tiny">
+    		<section class="appSection">
+          <h2 class="appSection__index">カテゴリー登録</h2>
+          		<section class="toCreateCategory childSection">
+                <h3 class="childSection__index">
+                  入力して追加する
+                </h3>
+                <div class="inputRow">
+                  <input type="text" name="name" placeholder="タスクの分類を入力してください（「アポ」「ランチ」…）" v-model="name">
+                  <button class="button button--primary" v-on:click="add()">送信</button>
+                </div>
+              </section>
+          		<section class="createdCategory childSection">
+                <h3 class="childSection__index">
+                  追加済みのカテゴリ
+                </h3>
+          			<ul class="labels">
+          				<li class="label label--category" v-for="(category) in this.categories" :key="category._id">
+          					{{ category.name }}
+          				</li>
+          			</ul>
+          		</section>
+        </section>
+    	</div>
+  </main>
 </template>
 
 <script>
@@ -52,5 +66,62 @@
   }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  /*******************
+  variables
+  *******************/
+
+  //////////////////////
+  // color
+  /////////////////////
+  $color_border: #BBBBBB;
+  $color_disabled: #bababa;
+  $color_primary: #22556E;
+  $paleblue: #F6FAFB;
+  $color_accent: #00D1E5;
+
+
+  /*******************
+  style
+  *******************/
+
+  .childSection{
+    margin-top: 40px;
+
+    &__index{
+      margin-top: 0;
+      font-size: 1em;
+      font-weight: lighter;
+    }
+  }
+
+  .toCreateCategory{
+
+    margin-top: 0;
+
+    .childSection__index{
+      padding-left: 20px;
+    }
+
+    .inputRow{
+      display: flex;
+
+      input{
+        width: 100%;
+      }
+
+      .button{
+        width: 100px;
+        margin-left: 20px;
+      }
+    }
+  }
+
+
+  .createdCategory{
+    padding: 20px;
+    background-color: $paleblue;
+  }
+
+
 </style>
