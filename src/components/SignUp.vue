bb237843520f06e3f4e4b366708f19816f4dbf2f
kabane <kabaneshi@gmail.com>
2019-02-24 13:49:40 +0900

    Login & SignUp用のコンポーネント追加

---
+++ src/components/SignUp.vue
@@ -0,0 +1,24 @@
+<template>
+	<main class="main">
+    <div>SignUp</div>
+  </main>
+</template>
+
+<script>
+	import axios from 'axios'
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'category',
+    components: {
+    },
+    data: function () {
+		},
+		computed: {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
