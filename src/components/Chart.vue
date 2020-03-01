e208a2a6144045cb695f12cd2cb4608b44b5048c
kabane <kabaneshi@gmail.com>
2020-03-01 18:00:21 +0900

    分析用のStore追加 & 設定周りの修正

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,49 +1,59 @@
 <script>
   import { mapGetters } from 'vuex'
   import VueCharts from 'vue-chartjs'
-  import { Bar, Line } from 'vue-chartjs'
+  import { Bar, mixins } from 'vue-chartjs'
   
   export default {
+    mixins: [Bar, mixins.reactiveData],
     name: 'chart',
-    extends: Line,
-  name: 'chart',
-  data () {
-    return {
-      data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-        datasets: [
-          {
-            label: 'Line Dataset',
-            data: [10, 50, 20, 30, 30, 40],
-            backgroundColor: '#f87979',
-            borderColor: '#f87979',
-            fill: false,
-            type: 'line',
-            lineTension: 0.3,
+    extends: Bar,
+    data () {
+      return {
+        chartData: {
+          labels: [],
+          datasets: [
+            {
+              label: '時間',
+              data: [],
+              backgroundColor: '#f87979',
+              borderColor: '#f87979',
+              fill: false,
+              lineTension: 0.3,
+            }
+          ]
+        },
+        options: {
+          scales: {
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'タスクカテゴリー'
+              }
+            }],
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+              }
+            }]
           }
-        ]
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            scaleLabel: {
-              display: true,
-              labelString: 'Month'
-            }
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-              stepSize: 10,
-            }
-          }]
         }
       }
+    },
+    computed: {
+      ...mapGetters({
+        labels: 'todo_summary/labels',
+        datum: 'todo_summary/datum'
+      })
+    },
+    mounted () {
+      this.$store.dispatch('todo_summary/fetch', {}).then(function(){
+        const chartData = Object.assign({}, this.chartData)
+        chartData.labels = this.labels
+        chartData.datasets[0].data = this.datum
+        this.renderChart(chartData, this.options)
+      }.bind(this))
+      
     }
-  },
-  mounted () {
-    this.renderChart(this.data, this.options)
-  }
   }
 </script>
 
