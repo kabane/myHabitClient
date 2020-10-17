06b1ce83e8878ff08619e60f7c3e35ca400b3c3c
Masayasu Satoh <satohmsys@gmail.com>
2020-10-17 23:27:13 +0900

    update css ヘッダーのmargin等

--- src/components/common/HeaderPartsSignIn.vue
+++ src/components/common/HeaderPartsSignIn.vue
@@ -15,8 +15,8 @@
         <div class="siteHeader__nav__link">
           <router-link class="siteHeader__nav__category" to="/categories" exact>カテゴリ設定</router-link>
         </div>
-        <div class="siteHeader__nav__link">
-          <a v-on:click.prevent="logout">ログアウト</a>
+        <div class="siteHeader__nav__link ">
+          <a  class="siteHeader__nav__logout" v-on:click.prevent="logout">ログアウト</a>
         </div>
       </nav>
     </div>
@@ -47,117 +47,123 @@
 
 <style lang='scss'>
 
-    .wrap{
-      position: relative;
-      z-index: 2;    
+  .wrap{
+    position: relative;
+    z-index: 2;    
 
-      @media screen and ( max-width: 768px ){
-        z-index: 10;
-      }
-    }
-
-  .siteHeader{
-    &__logoarea {
-      line-height: 1;
-      text-align: center;
-
-      &__description {
-        margin-bottom: 1px;
-        font-size: 10px;
-        color: #ffffff;
-      }
-    }
-
-    &__nav{
-      line-height: 1;
-        display: flex;
-
-       @media screen and ( max-width: 640px ){    
-        width: 100%;  
-        position: fixed;
-        z-index: 10;
-        bottom: 0;
-        left: 0;
-        justify-content: center;
-        background-color: #22556e;
-        font-size: 1.1rem;
-        letter-spacing: 0.05em;
-      }        
-
-
-      a{
-        color:#fff;
-        text-decoration: none;
-        display: flex;
-        align-items: center;
-
-         @media screen and ( max-width: 640px ){  
-          width: 100%;
-          flex-direction: column;
-          justify-content: center;
-          height: 60px;
-        }            
-
-
-        &:before{
-            content: " ";
-            width: 20px;
-            height: 20px;
-            background-position: center center;
-            background-size: cover;
-            background-repeat: no-repeat;
-            display: inline-block;
-            vertical-align: middle;
-            margin-right: 5px;
-
-            @media screen and ( max-width: 640px ){
-              width: 27px;
-              height: 27px;
-              display: block;
-              margin-right: auto;
-              margin-bottom: 5px;
-              margin-left: auto;
-            }            
-          }          
-      }
-
-
-
-      &__link{
-        @media screen and ( max-width: 640px ){  
-          width: 100%;
-
-          &:not( :first-child ){
-            border-left: 1px solid rgba(#fff, 0.2);
-          }            
-        }            
-        &:not( :last-child ){
-          margin-right: 40px;
-
-           @media screen and ( max-width: 640px ){   
-            margin-right: 0;
-          }            
-        }
-      }        
-
-
-      &__percentage{
-          &:before{
-            background-image: url(../../assets/img/icon-taskpercentage.svg);
-          }
-      }
-
-      &__task{
-          &:before{
-            background-image: url(../../assets/img/icon-task.svg);
-          }
-      }
-      
-      &__category{
-          &:before{
-            background-image: url(../../assets/img/icon-category.svg);
-          }
-      }
+    @media screen and ( max-width: 768px ){
+      z-index: 10;
     }
   }
+
+.siteHeader{
+  &__logoarea {
+    line-height: 1;
+    text-align: center;
+
+    &__description {
+      margin-bottom: 1px;
+      font-size: 10px;
+      color: #ffffff;
+    }
+  }
+
+  &__nav{
+    line-height: 1;
+      display: flex;
+
+      @media screen and ( max-width: 640px ){    
+      width: 100%;  
+      position: fixed;
+      z-index: 10;
+      bottom: 0;
+      left: 0;
+      justify-content: center;
+      background-color: #22556e;
+      font-size: 1.1rem;
+      letter-spacing: 0.05em;
+    }        
+
+
+    a {
+      color:#fff;
+      text-decoration: none;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+
+      @media screen and ( max-width: 640px ){  
+        width: 100%;
+        flex-direction: column;
+        justify-content: center;
+        height: 60px;
+      }        
+
+      &:before{
+        content: " ";
+        width: 20px;
+        height: 20px;
+        background-position: center center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        display: inline-block;
+        vertical-align: middle;
+        margin-right: 5px;
+
+        @media screen and ( max-width: 640px ){
+          width: 27px;
+          height: 27px;
+          display: block;
+          margin-right: auto;
+          margin-bottom: 5px;
+          margin-left: auto;
+        }            
+      }          
+    } 
+
+    &__link{
+      @media screen and ( max-width: 640px ){  
+        width: 100%;
+
+        &:not( :first-child ){
+          border-left: 1px solid rgba(#fff, 0.2);
+        }            
+      }   
+
+      &:not( :last-child ){
+        margin-right: 40px;
+
+          @media screen and ( max-width: 640px ){   
+          margin-right: 0;
+        }            
+      }
+    }       
+
+    &__logout {
+      padding-left: 20px;
+      border-left: solid 1px rgba( #fff ,0.2 );
+      
+      a:before {
+        display: none;
+      }
+    }       
+    &__percentage{
+      &:before{
+        background-image: url(../../assets/img/icon-taskpercentage.svg);
+      }
+    }
+
+    &__task{
+      &:before{
+        background-image: url(../../assets/img/icon-task.svg);
+      }
+    }
+    
+    &__category{
+      &:before{
+      background-image: url(../../assets/img/icon-category.svg);
+      }
+    }
+  }     
+}
 </style>
